42ccb05e85733136995d28cb8b0ead668a7f18c6
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-21 19:03:00 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description "Installs Oracle's Java SE JDK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '8.51.1'
+version '8.51.2'
 
 # supports 'centos'
 supports 'mac_os_x'
