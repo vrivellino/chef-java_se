9645b6b93ff681ef2a7f1bee924afe64d187c434
Dennis Hoer <dennis.hoer@pearson.com>
2015-09-10 22:28:44 -0700

    Test skip

---
+++ spec/unit/skip_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'java_se::default' do
+  context 'skip install' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.set['java_se']['skip'] = true
+      end.converge(described_recipe)
+    end
+
+    it 'does nothing' do
+      expect(chef_run).to_not include_recipe('java_se::_download_java')
+      expect(chef_run).to_not include_recipe('java_se::_linux_install')
+      expect(chef_run).to_not include_recipe('java_se::_macosx_install')
+      expect(chef_run).to_not include_recipe('java_se::_windows_install')
+    end
+  end
+end
