c7978fe22c819508a6ac96428a014a14570297a4
Dennis Hoer <dennis.hoer@pearson.com>
2016-01-15 17:34:34 -0700

    Share integration spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-BUILD = '17'
-VERSION_MAJOR = '8'
-VERSION_UPDATE = '66'
+BUILD = '17'.freeze
+VERSION_MAJOR = '8'.freeze
+VERSION_UPDATE = '66'.freeze
 
 ChefSpec::Coverage.start!
