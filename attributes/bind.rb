f08cf23852db3c4a47bb7a548f9b47fa636e59af
Dennis Hoer <dennis.hoer@pearson.com>
2016-02-10 12:41:59 -0600

    Update 74

--- attributes/bind.rb
+++ attributes/bind.rb
@@ -1,10 +1,10 @@
 default['java_se']['release'] = '8'
-default['java_se']['update'] = '73'
+default['java_se']['update'] = '74'
 default['java_se']['build'] = '02'
 
-# https://www.oracle.com/webfolder/s/digest/8u73checksum.html
-default['java_se']['sha256']['tar']['i586'] = 'f4f1f7ab1b5986aa2ee2a2f035a2d7a8ab57a673bdee4fc51d8127dd84f423ae'
-default['java_se']['sha256']['tar']['x64'] = '46a8149efaf9d87a147b244e503f1d7791ec8abbc35a5011c99ed909edb2e3e6'
-default['java_se']['sha256']['dmg']['x64'] = 'a96efc6aad18e3b49dd41f4b7a955f9ef2e01ee390987c63f5a6347026fcc336'
-default['java_se']['sha256']['exe']['i586'] = 'c98792ea96512b03238a60616a5cefe936832718d6453fff2a545615fbdf0d87'
-default['java_se']['sha256']['exe']['x64'] = 'a4e2f75ead7c5ab09a58b6d77ff98da4b1343caaab383c4b93479dd5866212ea'
+# https://www.oracle.com/webfolder/s/digest/8u74checksum.html
+default['java_se']['sha256']['tar']['i586'] = '0805e407924eb6902d874412af397992c141827e05c205c2b2bfe6e5127a83ed'
+default['java_se']['sha256']['tar']['x64'] = '0bfd5d79f776d448efc64cb47075a52618ef76aabb31fde21c5c1018683cdddd'
+default['java_se']['sha256']['dmg']['x64'] = '52fc1bf96514553cd70e5dfa79b764f8522f23536c05ad413c5585755f08e998'
+default['java_se']['sha256']['exe']['i586'] = '8ae01bd2d1c6e947cd47dec0a0b33560a67649b6a3044f4effd7b652394c7163'
+default['java_se']['sha256']['exe']['x64'] = 'a02623388258681c0e474d1b6598499ec1eb2bd5e4a32631e165f559d7b2389c'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'chefspec/berkshelf'
 
 BUILD = '02'.freeze
 VERSION_MAJOR = '8'.freeze
-VERSION_UPDATE = '73'.freeze
+VERSION_UPDATE = '74'.freeze
 
 ChefSpec::Coverage.start!
