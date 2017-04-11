88154d7a777ddee986e789d8c130733e1cc0be31
Dennis Hoer <dennis.hoer@pearson.com>
2017-04-11 16:51:58 -0700

    Add chef_version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description "Installs Oracle's Java SE JDK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/dhoer/chef-java_se' if respond_to?(:source_url)
+issues_url 'https://github.com/dhoer/chef-java_se/issues' if respond_to?(:issues_url)
+chef_version '>= 11.0' if respond_to?(:chef_version)
 version '8.121.0'
 
 supports 'centos'
@@ -14,5 +17,3 @@ supports 'redhat'
 supports 'ubuntu'
 supports 'windows'
 
-source_url 'https://github.com/dhoer/chef-java_se' if respond_to?(:source_url)
-issues_url 'https://github.com/dhoer/chef-java_se/issues' if respond_to?(:issues_url)
