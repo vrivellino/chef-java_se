2cb37e538c8e2a5e8b3975882d5f1e71e95c5264
Dennis Hoer <dennis.hoer@pearson.com>
2017-07-19 14:16:24 -0700

    Fix invalid linux checksum

--- attributes/bind.rb
+++ attributes/bind.rb
@@ -5,7 +5,7 @@ default['java_se']['hash'] = '336fa29ff2bb4ef291e347e091f7f4a7'
 
 # https://www.oracle.com/webfolder/s/digest/8u141checksum.html
 default['java_se']['sha256']['tar']['i586'] = '6562f2bc8865e9c693b04c591d86d121871f49463ab0ca83fa0bcb070ffe084b'
-default['java_se']['sha256']['tar']['x64'] = 'c3586f6ad0cb6cf4a00a0610fc50004370820b6021c58642de2873db99e9a6c9'
+default['java_se']['sha256']['tar']['x64'] = '041d5218fbea6cd7e81c8c15e51d0d32911573af2ed69e066787a8dc8a39ba4f'
 default['java_se']['sha256']['dmg']['x64'] = 'ed540bc452c1fd7772a27db6dc45b65d37ed0808edfa8853a234d3fc21e6992c'
 default['java_se']['sha256']['exe']['i586'] = 'f144354be4bc0df7bba6e97e5c2602b743a87604717d11417de6a1445cfab1e5'
 default['java_se']['sha256']['exe']['x64'] = '0e422f42b5085d463bb5096a77084de863c64618905d63e7133bbbb6958a23fd'
