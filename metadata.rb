f6ec45f11acc32a2c29326668e1570d936af97c8
Vincent Rivellino <github@vince-riv.io>
2018-04-03 16:45:58 -0400

    Update links in docs

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'java_se'
-maintainer 'Dennis Hoer'
-maintainer_email 'dennis.hoer@gmail.com'
+maintainer 'Vincent Rivellino'
+maintainer_email 'github@vince-riv.io'
 license 'MIT'
 description "Installs Oracle's Java SE JDK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/dhoer/chef-java_se' if respond_to?(:source_url)
-issues_url 'https://github.com/dhoer/chef-java_se/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/vrivellino/chef-java_se' if respond_to?(:source_url)
+issues_url 'https://github.com/vrivellino/chef-java_se/issues' if respond_to?(:issues_url)
 chef_version '>= 11.0' if respond_to?(:chef_version)
 version '9.0.4'
 
