7d69947307a98df10527c0be3f87192e68194c24
Sebastian Lehn <lehn@etracker.com>
2018-04-18 11:21:29 +0200

    New: Update Java 8 to Patch 171

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@ require 'chefspec/berkshelf'
 
 BUILD = '12'.freeze
 VERSION_MAJOR = '8'.freeze
-VERSION_UPDATE = '162'.freeze
-HASH = '0da788060d494f5095bf8624735fa2f1'.freeze
+VERSION_UPDATE = '171'.freeze
+HASH = '512cd62ec5174c3487ac17c61aaa89e8'.freeze
 
 CACHE = Chef::Config[:file_cache_path]
 
