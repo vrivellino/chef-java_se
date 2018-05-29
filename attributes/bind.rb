a3e59e36517fcadd14669b829d2a6840941c1d80
Vincent Rivellino <github@vince-riv.io>
2018-05-29 10:26:13 -0400

    Update Java 8 to Patch 172

--- attributes/bind.rb
+++ attributes/bind.rb
@@ -1,13 +1,13 @@
 # get details from download_url
-# http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
+# http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.tar.gz
 default['java_se']['release'] = '8'
-default['java_se']['update'] = '171'
+default['java_se']['update'] = '172'
 default['java_se']['build'] = '11'
-default['java_se']['hash'] = '512cd62ec5174c3487ac17c61aaa89e8'
+default['java_se']['hash'] = 'a58eab1ec242421181065cdc37240b08'
 
-# https://www.oracle.com/webfolder/s/digest/8u171checksum.html
-default['java_se']['sha256']['tar']['i586'] = '37896425b90ea323d841cb57784cec21822c38754e94e415b7f102b205c80536'
-default['java_se']['sha256']['tar']['x64'] = 'b6dd2837efaaec4109b36cfbb94a774db100029f98b0d78be68c27bec0275982'
-default['java_se']['sha256']['dmg']['x64'] = '00ccc048009e64e7e341d55d35c8519ab63ef5f86f0d73d4e823281d0b358d40'
-default['java_se']['sha256']['exe']['i586'] = 'fa5de8068571d40f01d6ce8040761b63b5e805aca584e8904cfd026c3fd09138'
-default['java_se']['sha256']['exe']['x64'] = '841b20e904b7f46fe7c8ce88bd35148e9663c750c8336286d0eb05a0a5b203f4'
+# https://www.oracle.com/webfolder/s/digest/8u172checksum.html
+default['java_se']['sha256']['tar']['i586'] = '0a4310d31246924d5c3cd161b9da7f446acef373e6484452c80de8d8519f5a33'
+default['java_se']['sha256']['tar']['x64'] = '28a00b9400b6913563553e09e8024c286b506d8523334c93ddec6c9ec7e9d346'
+default['java_se']['sha256']['dmg']['x64'] = '256acd1a6157e8c8d5413ff67eab138b959234c8ce4f25f1bb19aa9ea428e685'
+default['java_se']['sha256']['exe']['i586'] = '434d7cbe88e2a28ee37c7345b5d93810c98112908f7dd5a273a0611482898928'
+default['java_se']['sha256']['exe']['x64'] = '580bd9a6da5640661c4dc6ebdb3eac451dbc49f23635728116d90a4d164d3a0f'
