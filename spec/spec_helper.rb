13caca9fd39467f688c7e1c705aac9e9c4b9e064
Vincent Rivellino <github@vince-riv.io>
2018-07-19 08:21:20 -0400

    Java 8u181

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-BUILD = '11'.freeze
+BUILD = '13'.freeze
 VERSION_MAJOR = '8'.freeze
-VERSION_UPDATE = '172'.freeze
-HASH = 'a58eab1ec242421181065cdc37240b08'.freeze
+VERSION_UPDATE = '181'.freeze
+HASH = '96a7b8442fe848ef90c96a2fad6ed6d1'.freeze
 
 CACHE = Chef::Config[:file_cache_path]
 
