56c9532a609f2396882ac38761fca68fdf586eae
Vincent Rivellino <github@vince-riv.io>
2019-01-16 17:38:46 -0500

    Java 11.0.2

--- attributes/bind.rb
+++ attributes/bind.rb
@@ -1,9 +1,9 @@
-# http://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_osx-x64_bin.dmg
-default['java_se']['release'] = '11.0.1'
-default['java_se']['build'] = '13'
-default['java_se']['hash'] = '90cf5d8f270a4347a95050320eef3fb7'
+# https://download.oracle.com/otn-pub/java/jdk/11.0.2+7/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_osx-x64_bin.dmg
+default['java_se']['release'] = '11.0.2'
+default['java_se']['build'] = '7'
+default['java_se']['hash'] = 'f51449fcd52f4d52b93a989c5c56ed3c'
 
-# https://www.oracle.com/webfolder/s/digest/11-0-1checksum.html
-default['java_se']['sha256']['tar'] = 'e7fd856bacad04b6dbf3606094b6a81fa9930d6dbb044bbd787be7ea93abc885'
-default['java_se']['sha256']['dmg'] = '467fd06212438539078c56f768c42c41ba8b26cea1213991da5ba44a601af628'
-default['java_se']['sha256']['exe'] = '885a5b8ed0305fbd187d66b4b609550fc2bb488ab23983f803e21fea5cbdac61'
+# https://www.oracle.com/webfolder/s/digest/11-0-2checksum.html
+default['java_se']['sha256']['tar'] = '1f14572cae12adec64d28fdbf38935df7ced20b3efb54c33934b8bbecafdcd79'
+default['java_se']['sha256']['dmg'] = '4aa14260013df1ab31890d798cde774e220a73b5a9f99fd4f95757efc609feb5'
+default['java_se']['sha256']['exe'] = '4941b422267722a5ada6250d0a2f91bfa45ca6c7e6a5266dba492ad74ba0f905'
