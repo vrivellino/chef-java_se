dd239d8c80fe0f02e441dfee043fff6920465e4e
Michael Rush <rush@dfstudio.com>
2019-03-13 09:56:07 -0700

    Updated build number and checksums
    Updated build number from 7 to 9 for 11.0.2 release, updated checksums

--- attributes/bind.rb
+++ attributes/bind.rb
@@ -1,9 +1,9 @@
-# https://download.oracle.com/otn-pub/java/jdk/11.0.2+7/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_osx-x64_bin.dmg
+# https://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz
 default['java_se']['release'] = '11.0.2'
-default['java_se']['build'] = '7'
+default['java_se']['build'] = '9'
 default['java_se']['hash'] = 'f51449fcd52f4d52b93a989c5c56ed3c'
 
 # https://www.oracle.com/webfolder/s/digest/11-0-2checksum.html
-default['java_se']['sha256']['tar'] = '1f14572cae12adec64d28fdbf38935df7ced20b3efb54c33934b8bbecafdcd79'
-default['java_se']['sha256']['dmg'] = '4aa14260013df1ab31890d798cde774e220a73b5a9f99fd4f95757efc609feb5'
-default['java_se']['sha256']['exe'] = '4941b422267722a5ada6250d0a2f91bfa45ca6c7e6a5266dba492ad74ba0f905'
+default['java_se']['sha256']['tar'] = '7b4fd8ffcf53e9ff699d964a80e4abf9706b5bdb5644a765c2b96f99e3a2cdc8'
+default['java_se']['sha256']['dmg'] = '466935684e8772b8fdd9884fc24f7c342909abfc5aff07c908d248026edee737'
+default['java_se']['sha256']['exe'] = 'c808bcf758f36ebfae1513a7f326d0747e48044ee55831fbf0e12cbfc18b3950'
